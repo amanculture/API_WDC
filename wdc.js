0f583cf7312a983d8cd30c4684f2be37d53ba026
amanculture <Aman.culturehlidays@gmail.com>
2024-11-14 12:21:46 +0530

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -1,61 +1,48 @@
-(function() {
-    var myConnector = tableau.makeConnector();
+myConnector.getSchema = function (schemaCallback) {
+  var cols = [
+    { id: "id", alias: "ID", dataType: tableau.dataTypeEnum.string },
+    { id: "urn", alias: "URN", dataType: tableau.dataTypeEnum.string },
+    { id: "username", alias: "Username", dataType: tableau.dataTypeEnum.string },
+    { id: "first_name", alias: "First Name", dataType: tableau.dataTypeEnum.string }
+  ];
 
-    // Define the schema (structure of the data)
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [
-            { id: "id", alias: "ID", dataType: tableau.dataTypeEnum.string },
-            { id: "urn", alias: "URN", dataType: tableau.dataTypeEnum.string },
-            { id: "username", alias: "Username", dataType: tableau.dataTypeEnum.string },
-            { id: "firstname", alias: "First Name", dataType: tableau.dataTypeEnum.string },
-            // Add other fields from the LinkedIn API as needed
-        ];
+  var tableSchema = {
+    id: "linkedinData",
+    alias: "LinkedIn Data",
+    columns: cols
+  };
 
-        var tableSchema = {
-            id: "linkedinData",
-            alias: "LinkedIn Data from API",
-            columns: cols
-        };
+  schemaCallback([tableSchema]);
+};
+myConnector.getData = function (table, doneCallback) {
+  var apiKey = "8b2d55889bmshafe33f821c7a253p15f9bejsnd92ff5f9a206";  // Replace with your RapidAPI key
+  var apiUrl = "https://linkedin-data-api.p.rapidapi.com/";
 
-        schemaCallback([tableSchema]);
-    };
+  var endpoint = "your_endpoint"; // Replace with the correct endpoint
 
-    // Fetch data from the LinkedIn Data API using the API key
-    myConnector.getData = function(table, doneCallback) {
-        var apiKey = "8b2d55889bmshafe33f821c7a253p15f9bejsnd92ff5f9a206"; // Your API Key
-        var apiUrl = "https://linkedin-data-api.p.rapidapi.com/"; // Base URL
+  fetch(apiUrl + endpoint, {
+    method: 'GET',
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": "linkedin-data-api.p.rapidapi.com",
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Assuming the API response is in the format: { "data": [{...}] }
+    var tableData = data.data.map(item => ({
+      id: item.id,  // Assuming 'id' is present in the API response
+      urn: item.urn,  // Assuming 'urn' is present in the API response
+      username: item.username,  // Assuming 'username' is present
+      first_name: item.firstName  // Assuming 'firstName' is present
+    }));
 
-        // Add your specific API endpoint (e.g., search profiles, company data)
-        var endpoint = "some_endpoint"; // Modify with the correct endpoint
-
-        // Fetch data from the LinkedIn API
-        fetch(apiUrl + endpoint, {
-            method: 'GET',
-            headers: {
-                "X-RapidAPI-Key": apiKey,
-                "X-RapidAPI-Host": "linkedin-data-api.p.rapidapi.com",
-                "Content-Type": "application/json"
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            var tableData = data.map(item => ({
-                id: item.id,
-                urn: item.urn,
-                username: item.username,
-                firstname: item.firstname,
-                // Map other fields as needed
-            }));
-
-            // Append rows to Tableau
-            table.appendRows(tableData);
-            doneCallback();
-        })
-        .catch(error => {
-            console.error("Error fetching data: ", error);
-            doneCallback();
-        });
-    };
-
-    tableau.registerConnector(myConnector);
-})();
+    table.appendRows(tableData);
+    doneCallback();
+  })
+  .catch(error => {
+    console.error("Error fetching LinkedIn data: ", error);
+    doneCallback();
+  });
+};
